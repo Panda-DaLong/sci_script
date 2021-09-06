927bdafa67178a197da54a08baf8c20bb1d94336
Panda-DaLong <73022811+Panda-DaLong@users.noreply.github.com>
2021-09-06 16:31:38 +0800

    Add files via upload

---
+++ openmm/head/remove_hea_ch3_pdb.py
@@ -0,0 +1,80 @@
+import sys
+
+in_file = sys.argv[1]
+#in_file is good format pdb file by modi_gauss_pdb.py
+#this python script aims for removing cap group -CH3 in tail
+
+add_num_atom = -4 
+
+hetatm_total_num = 35
+
+ch3_atoms = []
+ch3_min_atom = 6
+ch3_max_atom = 9
+for i in range(ch3_min_atom, ch3_max_atom + 1):
+    ch3_atoms.append(str(i))
+
+def remove_ch3(line):
+    l_s = line.split()
+    for i in l_s:
+        if i in ch3_atoms:
+            return i
+            break
+
+def hetatm_change_atom_num(line_str, add_num):
+    atom = str(int(line_str[9:11]) + add_num)
+
+    if len(atom) == 1:
+        atom = ' ' + atom
+
+    new_line = line_str[:9] + atom + line_str[11:] 
+    return new_line
+
+def conect_change_atom_num(line, add_num):
+    l_s = line.split() 
+    atom1_index = int(l_s[1])
+    atom2_index = int(l_s[2])
+
+    if atom1_index > ch3_max_atom:
+        atom1_index = atom1_index + add_num
+    if atom2_index > ch3_max_atom: 
+        atom2_index = atom2_index + add_num 
+      
+    atom1 = str(atom1_index)
+    atom2 = str(atom2_index)
+    
+    if len(atom1) == 1:
+        atom1 = ' ' + atom1 
+    if len(atom2) == 1:
+        atom2 = ' ' + atom2
+    
+    new_line = 'CONECT  ' + atom1 + '  ' + atom2
+    return new_line
+
+with open(in_file) as f:
+#Part_1
+#keep the lines before -ch3    
+    for i in range(ch3_min_atom - 1):
+        line = f.readline()
+        print(line, end='')
+
+#remove the -ch3 hetatm lines
+    for i in range(4):
+        next(f)
+
+#change the hetatm lines after -ch3
+    for i in range(hetatm_total_num - ch3_max_atom):
+        line = f.readline()
+        line = hetatm_change_atom_num(line, add_num_atom) 
+        print(line, end='') 
+
+#Part_2
+#remove -ch3 conect
+#keep the others and change the indexs 
+    for line in f:
+        if remove_ch3(line):
+            print('', end='')
+        else:
+            line = conect_change_atom_num(line, add_num_atom)
+            print(line)
+

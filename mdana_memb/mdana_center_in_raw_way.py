2fb993bec9cf27a744fb44943731311fae857d1e
Panda-DaLong <73022811+Panda-DaLong@users.noreply.github.com>
2022-01-29 22:07:03 +0800

    Add files via upload

--- mdana_memb/mdana_center_in_raw_way.py
+++ mdana_memb/mdana_center_in_raw_way.py
@@ -56,18 +56,46 @@ def set_box(ts):
     ts.dimensions = all_box_dim[ts.frame]
     return ts
 
+def pbc_transform_atom(center_position, atom_position, pbc, axis_direction):
+    pbc_half = pbc / 2
+    if atom_position[axis_direction] > center_position[axis_direction] + pbc_half[axis_direction]:
+        return atom_position[axis_direction] - pbc[axis_direction]
+    elif atom_position[axis_direction] < center_position[axis_direction] - pbc_half[axis_direction]:
+        return atom_position[axis_direction] + pbc[axis_direction]
+
 
 u = mda.Universe(tra_file)
 
 u.trajectory.add_transformations(set_box)
 
 ag_cha_L = u.select_atoms('resname cha and chainID L')
-center_ag_cha_L = ag_cha_L.center_of_mass()
 
 
-for atom in u.atoms:
-    print(atom.position)
+for ts in u.trajectory:
+    print('**************')
+    print(ts.dimensions)
 
-with mda.Writer(cen_file, u.atoms.n_atoms) as w:
-    for ts in u.trajectory:
-        w.write(u)
+    center_ag_cha_L = ag_cha_L.center_of_mass()
+    print(center_ag_cha_L)
+
+prop [abs] property operator value
+与（中心+盒子一半）做比较，选中不同的区域作为原子组，对该原子组做translate变换
+
+    for atom in u.atoms:
+        print('@@@@@')
+        print(atom.name)
+        print(atom.position)
+
+#        for axis_direction in [0, 1, 2]:
+#            atom.position[axis_direction] = pbc_transform_atom(center_ag_cha_L, atom.position, ts.dimensions, axis_direction)
+#            atom.position[axis_direction] = 999
+
+#        atom.translate([-999, 0, 0])
+        print(atom.position)
+        print('$$$$$')
+    
+    print('**************')
+
+#with mda.Writer(cen_file, u.atoms.n_atoms) as w:
+#    for ts in u.trajectory:
+#        w.write(u)

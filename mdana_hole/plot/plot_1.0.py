ecd51ab54a19c685a48734728c29f45835e9c53e
Panda-DaLong <73022811+Panda-DaLong@users.noreply.github.com>
2022-02-15 23:54:28 +0800

    Add files via upload

---
+++ mdana_hole/plot/plot_1.0.py
@@ -0,0 +1,13 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+in_file = sys.argv[1]
+
+data = np.loadtxt(in_file)
+
+
+nums,bins,patches = plt.hist(data, bins=30, range=(0, 3), edgecolor='k', weights=[1/len(data)]*len(data))
+
+plt.show()
